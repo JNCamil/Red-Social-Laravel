2d3e9f1e79ece319ca67821713c003b050cbb152
JNCamil <juancamvh@hotmail.com>
2024-06-21 22:38:09 +0200

    bbdd y models

---
+++ app/Models/Image.php
@@ -0,0 +1,15 @@
+<?php
+//Para crear un modelo: php artisan make:model Image
+//Laravel usa el ORM -> ELOCUENCE
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+
+
+}
