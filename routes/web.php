151fd5d679c95d6ad924904021c3dc81c5100864
JNCamil <juancamvh@hotmail.com>
2024-06-23 18:43:51 +0200

    elementos del menú

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
